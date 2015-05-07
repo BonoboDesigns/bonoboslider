7ce25bc178536f5a000bd875c3171be703ec6f62
abraham <abe@unscene.us>
2015-05-06 18:41:58 -0700

    no message

--- wpbonoboville.sh
+++
@@ -1,3 +0,0 @@
-open -a Sublime\ Text\ 2 /code/dsbase/wpbonobos/wp-content/themes/fullby/ /code/dsbase/_partials.scss /code/dsbase/_commons.scss /code/dsbase/_netcolors.scss
-cd /code/dsbase/wpbonobos/wp-content/themes/fullby/
-sass _style.scss:style.css --sourcemap --watch
